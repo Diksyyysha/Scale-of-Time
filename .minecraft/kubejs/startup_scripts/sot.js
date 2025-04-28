4af251999626abb883a62a797104187c30fa71fe
Diksyyysha <mrdiksi135@gmail.com>
2025-04-29 00:54:49 +0500

    1

--- .minecraft/kubejs/startup_scripts/sot.js
+++ .minecraft/kubejs/startup_scripts/sot.js
@@ -3,3 +3,9 @@ StartupEvents.registry('item', event => {
       .displayName('§bScale of time') 
       .rarity('epic')
   })
+
+StartupEvents.registry('item', event => {
+    event.create('scale_of_time_1') 
+      .displayName('§bScale of time 2') 
+      .rarity('epic')
+  })
