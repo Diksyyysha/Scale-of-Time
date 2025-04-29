cfd1704f8e0ce7977564415ebe3350368ec0a247
Diksyyysha <mrdiksi135@gmail.com>
2025-04-29 16:06:41 +0500

    quests + kjs + update mod ftbquests

--- .minecraft/kubejs/startup_scripts/sot.js
+++ .minecraft/kubejs/startup_scripts/sot.js
@@ -1,3 +1,11 @@
+StartupEvents.registry('fluid', event => {
+  event.create('cum')
+	.displayName('Suspicious Milk')
+    .thinTexture(0xFFFFFF)
+    .bucketColor(0xFFFFFF)
+	.rarity('epic')
+})
+
 StartupEvents.registry('item', event => {
     event.create('scale_of_time') 
       .displayName('§bScale of time') 
